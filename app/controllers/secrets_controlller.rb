40e2006c44841135de1d6e5a78ee9b96baad674a
Justin Tooley <jstooley823@gmail.com>
2018-01-31 03:46:48 +0000

    automatically backed up by learn

--- app/controllers/secrets_controlller.rb
+++ app/controllers/secrets_controlller.rb
@@ -1,11 +1,6 @@
 class SecretsController < ApplicationController
-  before_action :require_login
+  before_action :require_logged_in
 
   def show
-  end
-  
-  private
-  def require_login
-    return head(:forbidden) unless session.include? :user_id
   end
 end
